811b9437a361a8da6fe3dd450aa5742c3e45c8ac
MUSAYEV07 <musayevotabek083@gmail.com>
2026-03-17 23:47:42 +0500

    ..../

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -3,7 +3,17 @@ import './Hero.css'
 
 function Hero() {
   return (
-    <div>Hero</div>
+    <section>
+        <div className="container">
+            <div className="hero-container">
+                <div className="hero-img">Images</div>
+                <div className="hero-content">
+                    <h1>Musayev Otabek</h1>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id sunt debitis voluptate.</p>
+                </div>
+            </div>
+        </div>
+    </section>
   )
 }
 
