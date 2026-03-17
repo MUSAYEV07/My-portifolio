214601ef3d677519be641c9078cdfc38d58382bd
MUSAYEV07 <musayevotabek083@gmail.com>
2026-03-18 00:00:44 +0500

    ...../

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,20 +1,37 @@
-import React from 'react'
-import './Hero.css'
+import "./Hero.css";
+import img from "./images.jpeg"
 
 function Hero() {
   return (
-    <section>
-        <div className="container">
-            <div className="hero-container">
-                <div className="hero-img">Images</div>
-                <div className="hero-content">
-                    <h1>Musayev Otabek</h1>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id sunt debitis voluptate.</p>
-                </div>
+    <section className="hero">
+      <div className="container">
+        <div className="hero-container">
+
+          <div className="hero-content">
+            <h1>
+              Hi, I'm <span>Your Name</span> 👋
+            </h1>
+
+            <h2>Frontend Developer</h2>
+
+            <p>
+              I build modern, responsive and user-friendly websites using React.
+            </p>
+
+            <div className="hero-buttons">
+              <a href="#" className="btn primary">Hire Me</a>
+              <a href="#" className="btn secondary">Download CV</a>
             </div>
+          </div>
+
+          <div className="hero-img">
+            <img src={img} alt="hero" />
+          </div>
+
         </div>
+      </div>
     </section>
-  )
+  );
 }
 
-export default Hero
+export default Hero;
