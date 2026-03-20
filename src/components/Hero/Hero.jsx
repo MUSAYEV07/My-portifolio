d20c30470ef57ebfe55c2aa4b72c3da1e715e4aa
MUSAYEV07 <musayevotabek083@gmail.com>
2026-03-20 17:19:47 +0500

    .../

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,28 +1,30 @@
 import React, { useEffect, useState } from "react";
 import "./Hero.css";
-import HeroImg from './images.jpeg'
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"/>
+import { FaYoutube, FaInstagram, FaTelegram } from "react-icons/fa";
+import HeroImg from "./images.jpeg";
+import "animate.css";
 
 function Hero() {
-
   const texts = ["Frontend Developer", "React Developer", "Web Designer"];
   const [text, setText] = useState("");
   const [index, setIndex] = useState(0);
   const [charIndex, setCharIndex] = useState(0);
 
   useEffect(() => {
-    if (charIndex < texts[index].length) {
-      setTimeout(() => {
+    const timeout = setTimeout(() => {
+      if (charIndex < texts[index].length) {
         setText((prev) => prev + texts[index][charIndex]);
         setCharIndex(charIndex + 1);
-      }, 100);
-    } else {
-      setTimeout(() => {
-        setText("");
-        setCharIndex(0);
-        setIndex((prev) => (prev + 1) % texts.length);
-      }, 1500);
-    }
+      } else {
+        setTimeout(() => {
+          setText("");
+          setCharIndex(0);
+          setIndex((prev) => (prev + 1) % texts.length);
+        }, 1500);
+      }
+    }, 100);
+
+    return () => clearTimeout(timeout);
   }, [charIndex, index]);
 
   return (
@@ -30,7 +32,7 @@ function Hero() {
       <div className="container">
         <div className="hero-container">
 
-          <div className="hero-content fade-in">
+          <div className="hero-content animate__animated animate__fadeInUp">
             <h1>
               Hi, I'm <span>Musayev Otabek</span> 👋
             </h1>
@@ -44,13 +46,22 @@ function Hero() {
             <div className="hero-buttons">
               <a href="#" className="btn primary">Read More</a>
             </div>
+
+            <div className="social-links">
+              <a href="https://www.youtube.com/" target="_blank" rel="noopener noreferrer">
+                <FaYoutube />
+              </a>
+              <a className="you" href="https://www.instagram.com/the.musayev.officeall/" target="_blank" rel="noopener noreferrer">
+                <FaInstagram />
+              </a>
+              <a href="https://t.me/Musayev_O_07" target="_blank" rel="noopener noreferrer">
+                <FaTelegram className="bb"/>
+              </a>
+            </div>
           </div>
 
-          <div className="hero-img fade-in">
-            <img 
-              src={HeroImg}
-              alt="hero"
-            />
+          <div className="hero-img animate__animated animate__fadeInRight">
+            <img src={HeroImg} alt="hero" />
           </div>
 
         </div>
